8463191ab3c79d4015370938532d6763414cee1c
yangyuqi@sina.com <yangyuqi@sina.com>
2018-07-11 19:47:51 +0800

    RNN_study_1 OK

--- RNN_study_1.py
+++ RNN_study_1.py
@@ -15,7 +15,7 @@ batch_size = 5
 num_batches = total_series_length//batch_size//truncated_backprop_length    
 
 '''
-return:
+return shape(batch_size, total_series_length / batch_size) = (5, 10000):
 x = array([[1, 1, 1, ..., 1, 1, 0],
        [1, 0, 1, ..., 0, 1, 1],
        [1, 0, 0, ..., 1, 1, 0],
@@ -31,29 +31,36 @@ y = array([[0, 0, 0, ..., 0, 0, 0],
 def generateData():    
     x = np.array(np.random.choice(2, total_series_length, p=[0.5, 0.5]))    
     y = np.roll(x, echo_step)    
-    y[0:echo_step] = 0    
+    # y[0:echo_step] = 0    
     x = x.reshape((batch_size, -1))  # The first index changing slowest, subseries as rows    
-    y = y.reshape((batch_size, -1))    
+    y = y.reshape((batch_size, -1))
+    y[:, :echo_step] = 0
     return (x, y)    
 
 
-batchX_placeholder = tf.placeholder(tf.float32, [batch_size, truncated_backprop_length])    
+batchX_placeholder = tf.placeholder(tf.float32, [batch_size, truncated_backprop_length])    # (5, 15)
 batchY_placeholder = tf.placeholder(tf.int32, [batch_size, truncated_backprop_length])    
 
-init_state = tf.placeholder(tf.float32, [batch_size, state_size])    
+init_state = tf.placeholder(tf.float32, [batch_size, state_size])   # (5, 4)
 
 
-W = tf.Variable(np.random.rand(state_size+1, state_size), dtype=tf.float32)    
-b = tf.Variable(np.zeros((1,state_size)), dtype=tf.float32)    
+W = tf.Variable(np.random.rand(state_size + 1, state_size), dtype=tf.float32)     # (5, 4)
+b = tf.Variable(np.zeros((1, state_size)), dtype=tf.float32)                      # (1, 4)
 
 
-W2 = tf.Variable(np.random.rand(state_size, num_classes),dtype=tf.float32)    
-b2 = tf.Variable(np.zeros((1,num_classes)), dtype=tf.float32)    
+W2 = tf.Variable(np.random.rand(state_size, num_classes),dtype=tf.float32)        # (4, 2)
+b2 = tf.Variable(np.zeros((1, num_classes)), dtype=tf.float32)                    # (1, 2)
 
 
 # Unpack columns    
-inputs_series = tf.unpack(batchX_placeholder, axis=1)    
-labels_series = tf.unpack(batchY_placeholder, axis=1)    
+# inputs_series = tf.unpack(batchX_placeholder, axis=1)    # 按列解包   (15, 5) 。tf.unpack 在 1.x 已被 tf.unstack 替代
+# labels_series = tf.unpack(batchY_placeholder, axis=1)    
+# inputs_series = tf.transpose(batchX_placeholder, perm=[1, 0])
+# labels_series = tf.transpose(batchY_placeholder, perm=[1, 0])    
+# inputs_series = np.transpose(batchX_placeholder)
+# labels_series = np.transpose(batchY_placeholder)    
+inputs_series = tf.unstack(batchX_placeholder, axis=1)
+labels_series = tf.unstack(batchY_placeholder, axis=1)
 
 
 # Forward pass    
@@ -62,56 +69,41 @@ states_series = []
 
 for current_input in inputs_series:    
     current_input = tf.reshape(current_input, [batch_size, 1])    
-    input_and_state_concatenated = tf.concat(1, [current_input, current_state])  # Increasing number of columns    
+    # input_and_state_concatenated = tf.concat(1, [current_input, current_state])  # Increasing number of columns    
+    input_and_state_concatenated = tf.concat([current_input, current_state], 1)
 
-#  循环外？
-next_state = tf.tanh(tf.matmul(input_and_state_concatenated, W) + b)  # Broadcasted addition    
-states_series.append(next_state)    
-current_state = next_state    
+    next_state = tf.tanh(tf.matmul(input_and_state_concatenated, W) + b)  # Broadcasted addition    
+    states_series.append(next_state)    
+    current_state = next_state    
 
-
+# loss
 logits_series = [tf.matmul(state, W2) + b2 for state in states_series] #Broadcasted addition    
-
 predictions_series = [tf.nn.softmax(logits) for logits in logits_series]    
 
-
-losses = [tf.nn.sparse_softmax_cross_entropy_with_logits(logits, labels) for logits, labels in zip(logits_series,labels_series)]    
-
+losses = [tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=labels) for logits, labels in zip(logits_series,labels_series)]    
 total_loss = tf.reduce_mean(losses)    
-
 
 train_step = tf.train.AdagradOptimizer(0.3).minimize(total_loss)    
 
 
 def plot(loss_list, predictions_series, batchX, batchY):    
-
     plt.subplot(2, 3, 1)    
     plt.cla()    
     plt.plot(loss_list)    
-
 
     for batch_series_idx in range(5):    
         one_hot_output_series = np.array(predictions_series)[:, batch_series_idx, :]    
         single_output_series = np.array([(1 if out[0] < 0.5 else 0) for out in one_hot_output_series])    
 
-
-    plt.subplot(2, 3, batch_series_idx + 2)    
-
-    plt.cla()    
-
-    plt.axis([0, truncated_backprop_length, 0, 2])    
-
-    left_offset = range(truncated_backprop_length)    
-
-    plt.bar(left_offset, batchX[batch_series_idx, :], width=1, color="blue")    
-
-    plt.bar(left_offset, batchY[batch_series_idx, :] * 0.5, width=1, color="red")    
-
-    plt.bar(left_offset, single_output_series * 0.3, width=1, color="green")    
-
+        plt.subplot(2, 3, batch_series_idx + 2)    
+        plt.cla()    
+        plt.axis([0, truncated_backprop_length, 0, 2])    
+        left_offset = range(truncated_backprop_length)    
+        plt.bar(left_offset, batchX[batch_series_idx, :], width=1, color="blue")    
+        plt.bar(left_offset, batchY[batch_series_idx, :] * 0.5, width=1, color="red")    
+        plt.bar(left_offset, single_output_series * 0.3, width=1, color="green")    
 
     plt.draw()    
-
     plt.pause(0.0001)    
 
 
@@ -123,12 +115,10 @@ with tf.Session() as sess:
     plt.show()    
     loss_list = []    
 
-
     for epoch_idx in range(num_epochs):    
         x, y = generateData()    
         _current_state = np.zeros((batch_size, state_size))    
         print("New data, epoch", epoch_idx)    
-
 
         for batch_idx in range(num_batches):    
             start_idx = batch_idx * truncated_backprop_length    
@@ -145,8 +135,10 @@ with tf.Session() as sess:
                 })    
 
             loss_list.append(_total_loss)    
-            if batch_idx%100 == 0:    
+
+            if batch_idx % 100 == 0:    
                 print("Step",batch_idx, "Loss", _total_loss)    
-                plot(loss_list, _predictions_series, batchX, batchY)    
-                plt.ioff()    
-                plt.show()    
+                plot(loss_list, _predictions_series, batchX, batchY)
+
+plt.ioff()
+plt.show()
